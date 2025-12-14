65ff7445b63e26894039aad5f448e6d86a7ce0f9
eunsi-lab <qazplm5996@gmail.com>
2025-12-14 21:00:56 +0900

    Update Works background bubbles to match user annotation

--- src/App.tsx
+++ src/App.tsx
@@ -281,18 +281,25 @@ function App() {
 
                 {/* Works Section with New Background Bubbles */}
                 <section id="work" style={{ padding: '2rem 3rem 10rem 3rem', position: 'relative' }}>
-                    {/* Richer Background decorations for Works section */}
-                    <FloatingBubble size="300px" color="rgba(160, 196, 255, 0.25)" top="5%" left="-10%" delay={0} duration={20} />
-                    <FloatingBubble size="400px" color="rgba(255, 198, 255, 0.2)" top="30%" left="85%" delay={5} duration={25} />
-                    <FloatingBubble size="250px" color="rgba(189, 178, 255, 0.25)" top="60%" left="5%" delay={2} duration={18} />
 
-                    {/* Small playful accents */}
-                    <FloatingBubble size="60px" color="#FFC6FF" top="15%" left="20%" delay={1} duration={12} />
-                    <FloatingBubble size="40px" color="#A0C4FF" top="25%" left="70%" delay={3} duration={15} />
-                    <FloatingBubble size="80px" color="#BDB2FF" top="45%" left="10%" delay={2} duration={14} />
-                    <FloatingBubble size="50px" color="#FFC6FF" top="75%" left="80%" delay={4} duration={16} />
-                    <FloatingBubble size="30px" color="#A0C4FF" top="85%" left="30%" delay={0} duration={10} />
-                    <FloatingBubble size="120px" color="rgba(255,255,255,0.6)" top="50%" left="50%" delay={6} duration={20} />
+                    {/* 1. Top Left Large Bubble (User's Red Circle 1) */}
+                    <FloatingBubble size="350px" color="rgba(160, 196, 255, 0.2)" top="-5%" left="-8%" delay={0} duration={18} />
+
+                    {/* 2. Middle Left Large Bubble (User's Red Circle 2) */}
+                    <FloatingBubble size="400px" color="rgba(255, 198, 255, 0.15)" top="40%" left="-12%" delay={2} duration={22} />
+
+                    {/* 3. Top Right Medium Bubble (User's Red Circle 3) */}
+                    <FloatingBubble size="300px" color="rgba(189, 178, 255, 0.2)" top="10%" left="88%" delay={1} duration={20} />
+
+                    {/* 4. Bottom Right Small/Medium Bubble (User's Red Circle 4) */}
+                    <FloatingBubble size="180px" color="rgba(160, 196, 255, 0.25)" top="75%" left="85%" delay={3} duration={15} />
+
+                    {/* Additional Playful Accents (Tiny dots) */}
+                    <FloatingBubble size="40px" color="#FFC6FF" top="20%" left="15%" delay={1} duration={12} />
+                    <FloatingBubble size="25px" color="#A0C4FF" top="30%" left="80%" delay={4} duration={14} />
+                    <FloatingBubble size="60px" color="#BDB2FF" top="55%" left="12%" delay={2} duration={16} />
+                    <FloatingBubble size="35px" color="#FFC6FF" top="85%" left="90%" delay={0} duration={10} />
+                    <FloatingBubble size="100px" color="rgba(255,255,255,0.7)" top="50%" left="50%" delay={5} duration={25} />
 
                     <div style={{ textAlign: 'center', marginBottom: '5rem', position: 'relative', zIndex: 2 }}>
                         <h2 style={{ fontSize: '3rem', fontWeight: 800, marginBottom: '1rem', letterSpacing: '-1px' }}>Playground</h2>
